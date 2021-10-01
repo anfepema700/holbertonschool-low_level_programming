f0adb069fd4d8b1c05912f838a58ca4f56ca3229
anfepema700 <anfepema700@gmail.com>
2021-10-01 00:13:10 -0700

    5 printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <studio.sh>
+#include <stdio.h>
+
 /**
- *main  - Entrada de punto
- *Return: always 0(success)
- **/
-int main (void)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
-printf "with proper grammar, but the outcome is a piece of art,";
-return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
