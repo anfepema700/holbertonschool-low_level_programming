c2d87d8ab9744c3477742b72fdebf85d770ff581
anfepema700 <anfepema700@gmail.com>
2021-10-01 00:08:27 -0700

    5 printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+include <studio.sh>
+/**
+ *main  - Entrada de punto
+ *Return: always 0(success)
+ **/
+int main (void)
+{
+printf "with proper grammar, but the outcome is a piece of art,";
+return (0);
+}
