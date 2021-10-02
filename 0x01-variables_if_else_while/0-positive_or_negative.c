46875d79e122d9ffec21fdba2b5324835178b0d5
anfepema700 <anfepema700@gmail.com>
2021-10-01 19:32:12 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,19 @@
 		int n;
        		srand(time(0));
 		n = rand() - RAND_MAX/2;
-			if(n>0){
-				printf("%d is positive\n",n);
-			}else if (n==0){
-				printf("%d is Zero\n",n);
-			}else{
-				printf("%d is negative\n",n);
-			}		
+
+if (n < 0)
+		{
+					printf("%d is %s\n", n, "negative");
+						}
+	else if (n > 0)
+			{
+						printf("%d is %s\n", n, "positive");
+							}
+		else
+				{
+							printf("%d is %s\n", n, "zero");
+								}
 			return (0);
-			}
+
+	}
