b33570adc6aa3b0731e3c038a21e5425b8b20092
anfepema700 <anfepema700@gmail.com>
2021-10-01 19:34:21 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main positivo, negativo o si es 0 el valor de la variable n
+ * main positivo, negativo o si es zero el valor de la variable n
  * return resultado de validación. 
  */
 	int main(void)
@@ -11,18 +11,17 @@
        		srand(time(0));
 		n = rand() - RAND_MAX/2;
 
-if (n < 0)
-		{
-					printf("%d is %s\n", n, "negative");
-						}
+	if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
 	else if (n > 0)
-			{
-						printf("%d is %s\n", n, "positive");
-							}
-		else
-				{
-							printf("%d is %s\n", n, "zero");
-								}
-			return (0);
-
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	return (0);
 	}
