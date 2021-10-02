e4708ec487f836cbdd919179736611612bc6bdaf
anfepema700 <anfepema700@gmail.com>
2021-10-01 20:16:08 -0700

    4 print alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,13 +2,13 @@
 /*
  *Main alphabet in lowercase, followed by a new line
  *return no imprimir q ni e
- **/
+**/
 int main(void)
 {
-	char a= 'a';
+	char a = 'a';
 
 	while (a <= 'z')
-	{	
+	{
 	if (a != 'q' && a != 'e')
 	{
 		putchar (a);
