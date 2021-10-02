7a58b7361ee36e61c939c0be8c35438072eecdc1
anfepema700 <anfepema700@gmail.com>
2021-10-01 20:23:31 -0700

    5 print numbers to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ **main - Print number 0 to 10
+ **
+ **Return: 0 success
+ **/
+
+	int main(void)
+	{
+	int a=0;
+	for (a =0;a>=10;a++ )
+	{
+	print (a);
+	}
+	return (0);
+	}
