b16fb81f33f9bddf3714c619fd2df52f64d479a6
anfepema700 <anfepema700@gmail.com>
2021-10-01 19:29:06 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,18 +5,17 @@
  * main positivo, negativo o si es 0 el valor de la variable n
  * return resultado de validación. 
  */
-
-int main(void)
-{
-	int n;
-       	srand(time(0));
-	n = rand()-RAND_MAX/2;
-	if(n>0){
-	printf("positivo:%d",n);
-	}else if (n==0){
-	printf("Zero: %d",n);
-	}else{
-	printf("negativo: %d",n);
-	}
-return (0);
-}
+	int main(void)
+	{
+		int n;
+       		srand(time(0));
+		n = rand() - RAND_MAX/2;
+			if(n>0){
+				printf("%d is positive\n",n);
+			}else if (n==0){
+				printf("%d is Zero\n",n);
+			}else{
+				printf("%d is negative\n",n);
+			}		
+			return (0);
+			}
