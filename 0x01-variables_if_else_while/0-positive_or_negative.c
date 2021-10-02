e3589827e66c8bd39790753e0bb81aec9e4c2dd4
anfepema700 <anfepema700@gmail.com>
2021-10-01 19:15:13 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.sh>
+/*
+ * main positivo o negativo
+ * return resultado de validación. 
+ */
+
+int main(void)
+{
+	int n=rand()- RAND_MAX/2;
+	if(n>0){
+	printf("El número es positivo:  %d",n);
+	}else if (n==0){
+	printf("el número es negativo: %d",n);
+	}else{
+	printf("El número es negativo: %d",n);
+	}
+return (0);
+}
