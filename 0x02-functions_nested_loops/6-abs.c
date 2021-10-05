9f5f5704e22de3c24ade87f3cd1fba73234ea8f3
anfepema700 <anfepema700@gmail.com>
2021-10-04 21:10:14 -0700

    6 abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ **main - Computes the absolute value of an integer
+ **
+ **Return: 0 success
+ **/
+
+
+
+int main(void)
+{
+	int val=5;
+	int x= abs(val);
+	
+	printf("%d", x);
+	return (0);
+}
