3d376167cdc3e814b75cba04510ea4766e34864a
anfepema700 <anfepema700@gmail.com>
2021-10-04 21:27:00 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ *  * jack_bauer - Print every minute of the day of Jack Bauer
+ *   * Description: Start from 00:00 to 23:59
+ *    */
+
+void main(void)
+{
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+
+	while (x < 1440)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(':');
+		putchar(c + '0');
+		putchar(d + '0');
+		putchar('\n');
+		d++;
+	if (d > 9)
+	{
+		d = 0;
+		c++;
+	}
+	if (c > 5)
+	{
+		c = 0;
+		b++;
+	}
+	if (b > 9)
+	{
+		b = 0;
+		a++;
+	}
+		x++;
+	}
+}
