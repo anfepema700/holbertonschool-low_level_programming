f63a5718f5529a9d1e6f7cf2ee7c25ca637c9448
anfepema700 <anfepema700@gmail.com>
2021-10-04 21:19:17 -0700

    7 last digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ **main - last digit 
+ **Returns: 0 success
+ **/
+
+int main(void)
+{
+	int num=5;
+	int LastDigit= num % 10;
+
+	printf("%d", LastDigit);
+	return (0);
+}
