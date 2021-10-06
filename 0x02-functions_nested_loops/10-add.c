710a08a775760d2bcfd6d200f26ccb1dbdada3cd
anfepema700 <anfepema700@gmail.com>
2021-10-05 17:32:03 -0700

    10 add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - add two number and show result
+ *@a: variable check
+ *@b: variable check
+ * Return: 0 success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
