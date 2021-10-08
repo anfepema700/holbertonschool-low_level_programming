03c5dffe7e82c523d302eb4ea74c49112b5554b2
anfepema700 <anfepema700@github.com>
2021-10-08 17:24:12 -0500

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+*_isupper - function checks for uppercase. 
+*@c: variable check.
+*return: 0 success 
+*/
+
+int _isupper(int c)
+{
+    if (c >='A' && c<='Z')
+    {
+        return(0);
+    }
+    else
+    {
+        return (1);
+    }
+}
