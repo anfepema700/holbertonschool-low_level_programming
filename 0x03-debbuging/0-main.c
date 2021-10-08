aced51267df9e5017b68df11c000051688c57826
anfepema700 <anfepema700@github.com>
2021-10-08 11:55:51 -0500

    prueba

---
+++ 0x03-debbuging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+    if (i>=0)
+    {
+        printf("%d es positivo",i);
+    }
+    else
+    {
+        printf("%d es negativo", i);
+    }
+}
