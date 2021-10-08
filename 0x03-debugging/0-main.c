776a0584cf4ff0453dce63ca0837c6fa18412e3e
anfepema700 <anfepema700@github.com>
2021-10-08 12:10:49 -0500

    modificación archivo main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-void positive_or_negative(int i)
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-    if (i>=0)
-    {
-        printf("%d es positivo",i);
-    }
-    else
-    {
-        printf("%d es negativo", i);
-    }
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
