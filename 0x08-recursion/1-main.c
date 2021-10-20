d7837f1f35162d2540d0fc6b2828b5cf75e65e1e
anfepema700 <anfepema700@github.com>
2021-10-20 11:02:47 -0700

    1-print_rev_recursion.c correction main.c

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,12 +1,13 @@
-#include "main.h"
+void _print_rev_recursion(char *);
 
 /**
- *  * main - check the code
+ *  * main - check the code for Holberton School students.
  *   *
  *    * Return: Always 0.
  *     */
 int main(void)
 {
-	    _print_rev_recursion("\nColton Walker");
-	        return (0);
+		_print_rev_recursion("\nFirst, solve the problem. Then, write the code");
+			return (0);
 }
+
