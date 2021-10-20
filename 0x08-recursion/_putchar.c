21460db280ecdbf9edc3e7088a557fdbf1763296
anfepema700 <anfepema700@github.com>
2021-10-20 10:16:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -7,9 +7,10 @@
 *
 *Return: on success 1.
 *on error, -1 is returned, and errno is seet appropriately.
-**/
+*/
+
 
 int _putchar(char c)
 {
-	return write(1. %c, 1);
+			return (write(1, &c, 1));
 }
