c7f99d52422708cf66a7918c06b162af13ca3633
anfepema700 <anfepema700@github.com>
2021-10-20 13:08:51 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,24 @@
 #include "main.h"
 
 /**
- *
- *
- *
- * /
+*_strlen_recursion - ceturn length of a string
+*@s: character ith a string
+*
+*Return: 0 success
+*/
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	i = 0;
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	else
+	{
+		i = 1 + _strlen_recursion(s + 1);
+	}
+	return (i);
+}
