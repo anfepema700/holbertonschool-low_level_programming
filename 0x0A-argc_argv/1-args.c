bf166580d976d56c975797f5be5da3abae6b84b6
anfepema700 <anfepema700@github.com>
2021-10-22 12:27:00 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - print number of elements in gcc
+*@argc:
+*@argv:
+*
+*Return: 0 success
+**/
+
+int main(int argc, int argv[])
+{
+	if (argv != 0)
+	{
+		_putchar(argc);
+	//	printf("Cantidad de elementos: %d\n",argc );
+	}
+	return (0);
+}
