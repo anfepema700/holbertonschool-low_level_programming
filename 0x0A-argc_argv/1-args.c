c5d8cd1d3fd6365b8e1dba4e1793295c72a67bce
anfepema700 <anfepema700@github.com>
2021-10-22 14:34:00 -0700

    2-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,18 +2,17 @@
 
 /**
 *main - print number of elements in gcc
-*@argc:
-*@argv:
+*@argc: character checker
+*@argv: character checker
 *
 *Return: 0 success
 **/
 
-int main(int argc, int argv[])
+int main(int argc, char **argv)
 {
 	if (argv != 0)
 	{
-		_putchar(argc);
-	//	printf("Cantidad de elementos: %d\n",argc );
+		printf("%d\n", argc);
 	}
 	return (0);
 }
