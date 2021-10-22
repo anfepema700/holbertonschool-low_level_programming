1d32d69c6a1b452c64952f5b6316ea4b097cc023
anfepema700 <anfepema700@github.com>
2021-10-22 14:36:52 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,16 +8,15 @@
 *Return: 0 success
 **/
 
-int main (int argc, char *argv[])
-{	
-	int i; 
+int main(int argc, char *argv[])
+{
+	int i;
 
-	for ( i = 0; i <= argc && argv[i] != NULL; i++)
-	{	
-		_putchar(argv[i]);
-		/*printf("Valor argc:[ %d ] valor argv: %s\n", i, argv[i]);*/
+	for (i = 0; i <= argc && argv[i] != NULL; i++)
+	{
+		printf("Valor argc:[ %d ] valor argv: %s\n", i, argv[i]);
 	}
-	return (0);	
+	return (0);
 }
 
 
