c2f7782ddd86cbac11ae3e04b7c14f413a6262e6
anfepema700 <anfepema700@github.com>
2021-10-22 11:58:34 -0700

    Configuración entorno del proyecto putchar y main.h

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+#include <unistd.h>
+/**
+ * *_putchar - writes the character c
+ * *@c: the character to print
+ * *
+ * *
+ * *Return: on success 1.
+ * *on error, -1 is returned, and errno is seet appropriately.
+ * */
+
+
+int _putchar(char c)
+{
+				return (write(1, &c, 1));
+}
