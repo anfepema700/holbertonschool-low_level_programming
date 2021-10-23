3a54fe1f99271bdb6197f93cbcbd664937dce9c5
anfepema700 <anfepema700@github.com>
2021-10-22 17:38:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*main - function for a multiplicate two numbers
+*@argc: character check
+*@argv: character check
+*
+* Return: 0 sucess
+**/
+
+
+int main(int argc, char* argv[])
+{	
+	int a;
+	int b; 
+
+	a = *argv[1];
+	b = *argv[2]; 
+		printf("1: %d 2: %d\n", a, b);
+	return (0);
+}
