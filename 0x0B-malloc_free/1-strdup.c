82738df32c1ff59cd2a06df545bf71ef4abf17e1
anfepema700 <anfepema700@github.com>
2021-10-27 08:11:55 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,14 +12,20 @@ char *_strdup(char *str)
 	int i, size;
 
 	if (str == NULL)
+	{
 	return (NULL);
+	}
 	for (size = 0; str[size] != '\0'; size++)
 	;
 		s = malloc((sizeof(char) * size) + 1);
 	if (s == NULL)
+	{
 	return (NULL);
+	}
 	for (i = 0; i < size; i++)
+	{
 		s[i] = str[i];
+	}
 		s[i] = '\0';
 	return (s);
 }
